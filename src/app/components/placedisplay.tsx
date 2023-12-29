bee5713de1245014775ae9f1b90b9f3292a2f988
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-12-28 17:29:14 -0800

    Card display working

--- src/app/components/placedisplay.tsx
+++ src/app/components/placedisplay.tsx
@@ -1,10 +1,24 @@
-export default function PlaceDisplay({data}:any) {
+export default function PlaceDisplay(props:any) {
 
-  const {name} = data
+  const {place, cards} = props
 
   return(
     <div>
-      <h1>{name}</h1>
+      <h1>{place}</h1>
+      <div>
+        {cards.cards.map((card:any) => <CardDisplay card={card} key={card.id} />)}
+      </div>
+    </div>
+  )
+}
+
+function CardDisplay(card:any) {
+
+  console.log(card.card)
+  return(
+    <div>
+      <h1>{card.card.id}:</h1>
+      <h1>${card.card.amount}</h1>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,20 @@ import PlaceDisplay from "./components/placedisplay";
 export default function Home() {
 
   const [places, setPlaces] = useState([])
-  const [data, setData] = useState([])
+  const [cards, setCards] = useState([])
   const [ loading, setLoading ] = useState(true)
 
   useEffect(() => {
     const getDBData = async () => {
       const querySnapshot = await getDocs(collection(db, "data"))
       let tempPlaces: any = []
-      let tempData:any = []
+      let tempCards:any = []
       querySnapshot.forEach((doc) => {
         tempPlaces.push(doc.id)
-        tempData.push(doc.data())
+        tempCards.push(doc.data())
       })
       setPlaces(tempPlaces)
-      setData(tempData)
+      setCards(tempCards)
     }
 
     getDBData()
@@ -29,14 +29,14 @@ export default function Home() {
   }, [])
 
   console.log(places)
-  console.log(data)
+  console.log(cards)
 
   return (
     <main className="flex flex-col justify-center items-center">
       <h1 className="my-5 text-3xl font-bold">Gift Card Tracker</h1>
       {loading ? <h1>loading...</h1> : 
       <div>
-        {places.map((place, index) => <PlaceDisplay place={place} data={data[index]} />)}
+        {places.map((place, index) => <PlaceDisplay place={place} cards={cards[index]} key={place} />)}
       </div>
       }
     </main>
